44971c1ec77fd132831fe19a3aa097fed6a3108e
XaRaBaS7 <massimiliano.barresi@gmail.com>
2026-08-25 21:27:22 +0200

    test: track gui v21 entrypoint

--- tests/test_gui_entrypoint.py
+++ tests/test_gui_entrypoint.py
@@ -4,5 +4,5 @@ from ps5_ffpfsc_renamer import gui
 
 
 def test_gui_entrypoint_tracks_latest_desktop_shell() -> None:
-    assert gui.RenamerApp.__module__.endswith("gui_v20")
+    assert gui.RenamerApp.__module__.endswith("gui_v21")
     assert gui.__all__ == ["RenamerApp", "main"]
